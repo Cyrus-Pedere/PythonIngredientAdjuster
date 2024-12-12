5779f7e2946bb179f3276f5272beec56ee7fa89c
Cyrus Pedere <148664282+Cyrus-Pedere@users.noreply.github.com>
2024-12-13 01:05:21 +0800

    Add files via upload

---
+++ Recipe.py
@@ -0,0 +1,18 @@
+#   Cyrus Jayson A. Pedere
+#   Bscpe 1-1 Programming Logic and Design
+#   FSw1 Module 2: Input, Processing, and Output
+# 
+#   **** 2. Ingredient Adjuster *****
+#         PYTHON LANGUAGE
+
+
+cookies = int(input("How many cookies you want to make?"))
+
+sugar = (1.5*cookies)/48
+butter = (1*cookies)/48
+flour = (2.75*cookies)/48
+
+print("The total number of cookies you want to make is: ", cookies, ".")
+print("The number of cups of sugar needed is:", sugar)
+print("The number of cups of butter needed is:", butter)
+print("The number of cups of flour needed is:", flour)
